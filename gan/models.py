6aca0aeb9cac441db205e3e3b23491b7d3119591
vporwal3 <98642342+vporwal3@users.noreply.github.com>
2023-05-06 23:22:49 -0500

    Update models.py

--- gan/models.py
+++ gan/models.py
@@ -7,16 +7,16 @@ class Discriminator(nn.Module):
     
         self.layers = nn.Sequential(
             nn.Conv2d(input_channels, 128, kernel_size=4, stride=2, padding=1),
-            nn.LeakyReLU(0.2),
+            nn.ReLU(0.2),
             nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.2),
+            nn.ReLU(0.2),
             nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.2),
+            nn.ReLU(0.2),
             nn.Conv2d(512, 1024, kernel_size=4, stride=2, padding=1),
             nn.BatchNorm2d(1024),
-            nn.LeakyReLU(0.2),
+            nn.ReLU(0.2),
             nn.Conv2d(1024, 1, kernel_size=4, stride=1, padding=1)
         )
     
