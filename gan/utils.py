c6ab5f475c5fe87879811e398a10f12e225b6475
vporwal3 <98642342+vporwal3@users.noreply.github.com>
2023-05-06 23:42:41 -0500

    Add files via upload

--- gan/utils.py
+++ gan/utils.py
@@ -5,39 +5,11 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
 
-def show_images(images, color=False):
-    if color:
-        sqrtimg = int(np.ceil(np.sqrt(images.shape[2]*images.shape[3])))
-    else:
-        images = np.reshape(images, [images.shape[0], -1])  # images reshape to (batch_size, D)
-        sqrtimg = int(np.ceil(np.sqrt(images.shape[1])))
-    sqrtn = int(np.ceil(np.sqrt(images.shape[0])))
-
-
-    fig = plt.figure(figsize=(sqrtn, sqrtn))
-    gs = gridspec.GridSpec(sqrtn, sqrtn)
-    gs.update(wspace=0.05, hspace=0.05)
-
-    for i, img in enumerate(images):
-        ax = plt.subplot(gs[i])
-        plt.axis('off')
-        ax.set_xticklabels([])
-        ax.set_yticklabels([])
-        ax.set_aspect('equal')
-        if color:
-            plt.imshow(np.swapaxes(np.swapaxes(img, 0, 1), 1, 2))
-        else:
-            plt.imshow(img.reshape([sqrtimg,sqrtimg]))
-    return 
-
 def preprocess_img(x):
-    return 2 * x - 1.0
+    return 2 * x - 1.0  #scales the pixel values from the range [0, 1] to the range [-1, 1] to improve stability
 
 def deprocess_img(x):
-    return (x + 1.0) / 2.0
-
-def rel_error(x,y):
-    return np.max(np.abs(x - y) / (np.maximum(1e-8, np.abs(x) + np.abs(y))))
+    return (x + 1.0) / 2.0 
 
 def sample_noise(batch_size, dim):
     """
